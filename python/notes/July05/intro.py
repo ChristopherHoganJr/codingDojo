25b9d2ba1367360d169ec51d3d3214978760f791
chris hogan <christopherhoganjr@icloud.com>
2022-07-05 11:21:36 -0700

    added morning notes -intro.py

---
+++ python/notes/July05/intro.py
@@ -0,0 +1,59 @@
+# print('hey there!')
+
+## defining a variable
+# hello_string = 'hey it\'s me!'
+## python version of console.log
+# print(hello_string)
+
+# def say_phrase(name):
+#     if len(name) >= 20:
+#         print(f'Greetings from a faraway land, fine sir/madam/knighted honorable {name}')
+#     elif len(name) > 10:
+#         print(f'Salutations, fine {name}')
+#     else:
+#         print(f'hello, {name}')
+
+# say_phrase('Christopher')
+
+# Arrays
+# nums = [4,8,15,16,23,42]
+# print(nums[2])
+
+## Dictionaries
+# sundae = {
+#     'flavor': 'vanilla',
+#     'sauce': 'raspberry syrup',
+#     'scoops': 10,
+#     'toppings': ['peanuts', 'chocolate shavings', 'whipped cream', 'cherry'],
+# }
+
+# print(sundae['toppings'])
+
+## Loops
+# count = 0
+# while(count < 10):
+#     print(count)
+#     count += 1
+
+
+
+## For N Loop
+# nums = [ 4,6,8,10,12,14]
+# for num in nums:
+#     num *= 2
+#     print(num)
+
+# print(nums)
+
+## For Range Loop = for i in range(start,end,step)
+# for num in range(1, -101, -5):
+#     print(num)
+
+### Javascript
+# for(var i = 0; i < 100; i++) {
+#   block body
+# }
+
+### Python
+# for i in range(100):
+#     block body

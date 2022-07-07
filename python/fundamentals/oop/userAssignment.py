3aa84955325cc4adc4c31f0de9380221483be939
Chris <55262451+ChristopherHoganJr@users.noreply.github.com>
2022-07-06 20:09:26 -0700

    Completed all challenges for Users with Bank Accounts

--- python/fundamentals/oop/userAssignment.py
+++ python/fundamentals/oop/userAssignment.py
@@ -8,35 +8,36 @@ class User:
         self.age = age
         self.is_rewards_member = False
         self.gold_card_points = 0
-        self.accounts = []
+        self.accounts = {}
 
     def create_new_account(self):
-        self.accounts.append(BankAccount(0.2,1000))
+        counter = len(self.accounts) + 1
+        globals()[f'Bank Account {counter}'] = BankAccount(1,100)
+        self.accounts[f'Account {counter}'] = globals()[f'Bank Account {counter}']
         return self
 
     def make_deposit(self, amount):
         if len(self.accounts) < 1:
             print('You can not deposit money until you make an account.')
         else:
-            for i in range(len(self.accounts)):
-                print(f'Account {i+1}')
-            accountToDeposit = int(input('Which account would you like to deposit money to: '))
-            self.accounts = self.accounts[accountToDeposit-1].deposit(amount)
+            print('There is no 0 index')
+            accountAccess = input(f'You have {len(self.accounts)} accounts. Which index account would you like to access?: ')
+            self.accounts[f'Account {accountAccess}'] = self.accounts[f'Account {accountAccess}'].deposit(amount)
         return self
 
     def make_withdrawal(self,amount):
-        self.accounts = self.account.withdraw(amount)
         if len(self.accounts) < 1:
             print('You can not withdraw money until you make an account.')
         else:
-            for i in range(len(self.accounts)):
-                print(f'Account {i+1}')
-            accountToDeposit = int(input('Which account would you like to deposit money to: '))
-            self.accounts = self.accounts[accountToDeposit-1].deposit(amount)
+            print('There is no 0 index')
+            accountAccess = input(f'You have {len(self.accounts)} accounts. Which index account would you like to access?: ')
+            self.accounts[f'Account {accountAccess}'] = self.accounts[f'Account {accountAccess}'].withdraw(amount)
         return self
 
     def display_user_balance(self):
-        print(self.accounts.balance)
+        print('There is no 0 index')
+        accountAccess = input(f'You have {len(self.accounts)} accounts. Which index account would you like to access?: ')
+        print(self.accounts[f'Account {accountAccess}'].balance)
 
     def display_info(self):
         print(f'First name: {self.first_name}')
@@ -62,8 +63,22 @@ class User:
             print("You don't have enough points")
         return self
 
+    def transfer_money(self, amount, other_user):
+        print('There is no 0 index')
+        selfAccess = input(f'You have {len(self.accounts)}. Which index account would you like to transfer money from?: ')
+        otherAccess = input(f'{other_user.first_name} has {len(other_user.accounts)} accounts. Which index account would you like to transfer money into?: ')
+        self.accounts[f'Account {selfAccess}'].withdraw(amount)
+        other_user.accounts[f'Account {otherAccess}'].deposit(amount)
+
 chris = User('chris','hogan','ch@asdf.com', 32)
-chris.create_new_account().create_new_account().create_new_account().make_deposit(100)
+batman = User('bruce','wayne','batman@dc.com',35)
+chris.create_new_account().create_new_account().create_new_account().make_deposit(50).display_user_balance()
+batman.create_new_account().create_new_account().create_new_account().make_deposit(50).display_user_balance()
+
+chris.transfer_money(150,batman)
+
+chris.display_user_balance()
+batman.display_user_balance()
 # chris.display_info().enroll().spend_points(50).display_info()
 # batman = User('bruce','wayne','batman@dc.com', 48)
 # superman = User('clark','kent','superman@dc.com', 80)
